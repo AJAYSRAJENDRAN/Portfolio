4b50b218df165d98e816c66ae935375c2766b0c7
AJAYSRAJENDRAN <ajayharigovindam@gmail.com>
2024-01-31 15:47:48 +0530

    fifth commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -255,9 +255,9 @@ function Hero() {
             data-aos="zoom-in"
             data-aos-duration="2500"
           >
-            <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto max-w-7xl px-6 lg:px-8 w-full h-full flex flex-col justify-center items-center rounded-md py-4 px-4 border-2 border-gray-600 lg:border-none">
               <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-                <div class="lg:pr-8 lg:pt-4">
+                <div class="lg:pr-8 lg:pt-4 ">
                   <div class="lg:max-w-lg">
                     <p class="mt-2 text-3xl font-bold tracking-tight text-red-600 sm:text-4xl">
                       ONE FOR ALL
@@ -278,7 +278,8 @@ function Hero() {
                 />
               </div>
             </div>
-            <div class="mx-auto max-w-7xl px-6 lg:px-8 mt-32">
+          
+            <div class="mx-auto max-w-7xl px-6 lg:px-8 mt-32  w-full h-full flex flex-col justify-center items-center rounded-md py-4 px-4 border-2 border-gray-600 lg:border-none">
               <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
                 <div class="lg:pr-8 lg:pt-4">
                   <div class="lg:max-w-lg">
@@ -302,7 +303,7 @@ function Hero() {
                 />
               </div>
             </div>
-            <div class="mx-auto max-w-7xl px-6 lg:px-8 mt-32">
+            <div class="mx-auto max-w-7xl px-6 lg:px-8 mt-32  w-full h-full flex flex-col justify-center items-center rounded-md py-4 px-4 border-2 border-gray-600 lg:border-none">
               <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
                 <div class="lg:pr-8 lg:pt-4">
                   <div class="lg:max-w-lg">
